43c73c4fc7aa55801af8efb5502e9e310ecb5c07
codyowl <codyowl@gmail.com>
2016-11-05 23:55:41 +0530

    colors added java checker done(still needs verfication :rocket:

--- alfred.py
+++ alfred.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 from os.path import expanduser
+from termcolor import colored
+
 
 HOME_PATH = expanduser("~/")
 
@@ -18,32 +20,45 @@ Please select a thing from the following to begin :
 """)
 
 def installation_checker(tool):
-	if tool == 'java':
-		command = '%s -version' % (tool)
-		command_executer = os.system(command)
-	else:   
-		command = '%s --version' % (tool)
-		command_executer = os.system(command)
+    if tool == 'java':
+        command = '%s -version' % (tool)
+        command_executer = os.system(command)
+    else:   
+        command = '%s --version' % (tool)
+        command_executer = os.system(command)
 
-	if command_executer != '0':
-		print "%s is not installed,let me install that for you master Bruce" % (tool)
+    if command_executer !=0:
+        print colored("%s is not installed,let me install that for you master bruce" % (tool), 'blue')
+        installer(tool=tool) #installer to install the things.
+    else:
+        print colored("%s is already installed master bruce" % (tool), 'blue') 
 
-def installer(tool):
-	if tool == 'java':
-		command = 'sudo apt-get install default-jre'			
 
-	
+def installer(tool=None):
+    if tool == 'java':
+        print colored("Installing java master bruce.....", "green")
+        command = 'sudo apt-get install default-jre'    
+        os.system(command)
+        print "Installing eclipse master bruce....."
+        # command = 
+
+# def ide_installer(tool=None):
+#     if tool == 'java':
+#         print colored('CHECKING WHETHER ECLIPSE IS INSTALLED OR NOT, MASTER BRUCE', 'green')
+#         command = 
+
+    
 if TOOLS == '1':
-	print installation_checker("java")   
+    print installation_checker("java")   
 
 elif TOOLS == '2':
-        print installation_checker("Android")
+    print installation_checker("Android")
 
 elif TOOLS == '3':
-        print installation_checker("python")
+    print installation_checker("python")
 
 elif TOOLS == '4':
-        print installation_checker("webdevelopment")
+    print installation_checker("webdevelopment")
 
 
 def installation_notes_creator(tool, version):
@@ -56,11 +71,11 @@ def installation_notes_creator(tool, version):
     file_opener.close()
 
 
-def installation_directory_checker(folder, home_path, installation_folder):
-    if not os.path.exists(folder):
-        director_creater = os.makedirs(os.path.join(HOME_PATH, INSTALLATION_FOLDER)
-    else:
-        print "Installation notes will be in %s as %s" % (installation_folder, installation_file)
+#def installation_directory_checker(folder, home_path, installation_folder):
+#    if not os.path.exists(folder):
+#        director_creater = os.makedirs(os.path.join(HOME_PATH, INSTALLATION_FOLDER)
+    #else:
+    #    print "Installation notes will be in %s as %s" % (installation_folder, installation_file)
 
        
         
