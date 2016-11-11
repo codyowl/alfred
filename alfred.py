a853f63cfe896563328a29b68361c0ade2715f26
codyowl <codyowl@gmail.com>
2016-11-11 12:59:24 +0530

    Username added instead of master bruce

--- alfred.py
+++ alfred.py
@@ -3,7 +3,10 @@ import subprocess
 import time
 from os.path import expanduser
 from termcolor import colored
+import getpass
 
+
+USER_NAME = getpass.getuser()
 
 HOME_PATH = expanduser("~/")
 
@@ -17,12 +20,12 @@ JAVA_IDE = "eclipse"
 
 TOOLS = raw_input(
 """
-Please select a thing from the following for setting up the environment:
+Please select a thing from the following to begin setting up the environment master %s :
 1.Java
 2.Android
 3.python
 4.Apache-tomcat
-""")
+"""% (USER_NAME)) 
 
 def installation_checker(tool):
     if tool == 'java':
@@ -30,16 +33,16 @@ def installation_checker(tool):
         command = '%s -version' % (tool)
         command_executer = os.system(command)
         if command_executer !=0:
-            print colored("%s is not installed,let me install that for you master bruce" % (tool), 'blue')
+            print colored("%s is not installed,let me install that for you master %s" % (tool, USER_NAME), 'blue')
             installer(tool=tool) 
         else:
-            print colored("%s is already installed master bruce" % (tool), 'blue') 
+            print colored("%s is already installed master %s" % (tool,USER_NAME), 'blue') 
         time.sleep(2)
         #checking IDE
         command = '%s --version' % (JAVA_IDE)
         command_executer = os.system(command)
         if command_executer !=0:
-            print colored("%s is not installed,let me install that for you master bruce" % (JAVA_IDE), 'blue')
+            print colored("%s is not installed,let me install that for you master %s" % (JAVA_IDE, USER_NAME), 'blue')
             installer(tool=JAVA_IDE) 
 
         installation_notes_creator(tool=tool, directory=INSTALLATION_FOLDER, file=INSTALLATION_DETAILS_FILE)     
@@ -53,23 +56,23 @@ def installation_checker(tool):
     elif tool == 'python':
         #checking pip
         time.sleep(2)
-        print colored("Checking pip... master bruce", "green")
+        print colored("Checking pip... master %s", "green") % (USER_NAME)
         pip_command = "pip list"
         pip_command_executer = os.system(pip_command)
         if pip_command_executer != 0:
-            print colored("Pip is not installed, let me install that for you master bruce", 'blue')
+            print colored("Pip is not installed, let me install that for you master %s", 'blue')%(USER_NAME)
             installer(tool='pip')
 
         #checking django
         time.sleep(2)
-        print colored("Checking django .... master bruce", "green")
+        print colored("Checking django .... master %s", "green") % (USER_NAME)
         django_command = "%s -c 'import django; print(django.get_version())'" % (tool)
         django_command_executer = os.system(django_command)
         if django_command_executer !=0:
-            print colored("Django is not installed, let me install that for you master bruce", 'blue')
+            print colored("Django is not installed, let me install that for you master %s", 'blue') % (USER_NAME)
             installer(tool='django')
         else:
-            print colored("Django is already here master bruce.. you can check the installation notes for further details", 'green') 
+            print colored("Django is already here master %s.. you can check the installation notes for further details", 'green') % (USER_NAME)
         installation_notes_creator(tool=tool, directory=INSTALLATION_FOLDER, file=INSTALLATION_DETAILS_FILE)   
     #for tomcat 
     elif tool == 'Apache-tomcat':
@@ -82,17 +85,17 @@ def installation_checker(tool):
 #Installer function
 def installer(tool=None):
     if tool == 'java':
-        print colored("Installing java master bruce.....", "green")
+        print colored("Installing java master %s.....", "green") % (USER_NAME)
         time.sleep(3)
         command = 'sudo apt-get install default-jre'    
         os.system(command)
     elif tool == 'eclipse':
-        print colored("Installing eclipse master bruce....", "green")
+        print colored("Installing eclipse master %s....", "green") % (USER_NAME)
         time.sleep(3)
         command = 'sudo apt-get install eclipse'    
         os.system(command)
     elif tool == 'Android':
-        print colored("Installing android studion master bruce....", "green")
+        print colored("Installing android studion master %s....", "green") % (USER_NAME)
         time.sleep(3)
         repository_command = 'sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make'
         update_command = 'sudo apt-get update'
@@ -103,21 +106,21 @@ def installer(tool=None):
         os.system(ubuntu_make_command)
         os.system(studio_command)    
     elif tool == 'Apache-tomcat':
-        print colored("Installing apache tomcat server master bruce...", "green")
+        print colored("Installing apache tomcat server master %s...", "green") % (USER_NAME)
         time.sleep(3)
         update_command = 'sudo apt-get update'
         tomcat_command = 'sudo apt-get install tomcat7'
         os.system(update_command)
         os.system(tomcat_command)
     elif tool == 'pip':
-        print colored("Installing pip master bruce.....", "green")
+        print colored("Installing pip master %s.....", "green") % (USER_NAME)
         time.sleep(3)
         update_command = 'sudo apt-get update'
         pip_command = 'sudo apt-get -y install python-pip'
         os.system(update_command)
         os.system(pip_command)     
     elif tool == 'django':
-        print colored("Installing django master bruce.....", "green")
+        print colored("Installing django master %s.....", "green") % (USER_NAME)
         time.sleep(3)
         command = 'pip install django'
         os.system(command) 
@@ -143,7 +146,7 @@ def installation_notes_creator(tool=None, directory=None, file=None):
     try:
         if not os.path.exists(directory):
             time.sleep(2)
-            print colored("Creating directory for installation notes master bruce", "green")
+            print colored("Creating directory for installation notes master %s", "green") % (USER_NAME)
             os.makedirs(os.path.join(HOME_PATH, directory))
             time.sleep(2)
             print colored("Directory named '%s' created, all the informations regarding installations will be in a file named '%s'" % (directory,file), "magenta") 
@@ -167,8 +170,8 @@ elif TOOLS == '2':
     print installation_checker("Android")
 
 elif TOOLS == '3':
-    print colored("Python is already here... master bruce", "blue")
-    print installation_checker("python") 
+    print colored("Python is already here... master %s", "blue") % (USER_NAME)
+    print installation_checker("python")  
     # path_checker_and_setter("python")
 
 elif TOOLS == '4':
